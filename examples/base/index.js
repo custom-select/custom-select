e5334103ec7a84959d43d9293ac7cfeaa451c666
Gionatan Lombardi <gionatan.lombardi@gmail.com>
2017-11-23 17:31:12 +0100

    created index.js

---
+++ examples/base/index.js
@@ -0,0 +1,6 @@
+import customSelect from "custom-select";
+import cssImport from './node_modules/custom-select/build/custom-select.css'
+
+const mySelects = customSelect("select");
+
+console.log(mySelects);
